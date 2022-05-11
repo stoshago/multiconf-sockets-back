706a1895b77a5c357eda71c839bb87e130e3be1c
Roman Dashkivskyy <mineholst@gmail.com>
2022-05-11 16:23:15 +0300

    Minor updates. During requests handling, jackson won't be able to parse requests without default constructor

--- src/main/java/ua/nix/multiconfback/api/request/CreateTodoListRequest.java
+++ src/main/java/ua/nix/multiconfback/api/request/CreateTodoListRequest.java
@@ -3,9 +3,11 @@ package ua.nix.multiconfback.api.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateTodoListRequest {
 
     private String title;
--- src/main/java/ua/nix/multiconfback/api/request/SignupRequest.java
+++ src/main/java/ua/nix/multiconfback/api/request/SignupRequest.java
@@ -2,9 +2,11 @@ package ua.nix.multiconfback.api.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignupRequest {
 
     private String username;
