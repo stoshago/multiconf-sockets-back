7bda4fa3ed9e65cfe1cb63db07ce92b451344abb
Roman Dashkivskyy <mineholst@gmail.com>
2022-05-11 16:34:14 +0300

    Add CamelRouter reading and logging messages

---
+++ src/main/java/ua/nix/multiconfback/amq/SocketCamelRoute.java
@@ -0,0 +1,24 @@
+package ua.nix.multiconfback.amq;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class SocketCamelRoute extends RouteBuilder {
+
+    @Value("${amq.broker.topic}")
+    private String topic;
+
+    @Override
+    public void configure() {
+        from("activemq:topic:" + topic)
+                .log(">>> Read message: ${body}");
+
+    }
+
+}
