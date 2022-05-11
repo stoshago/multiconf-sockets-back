764c99d8673e83d86166773d780678365d7d2f77
Roman Dashkivskyy <mineholst@gmail.com>
2022-05-11 16:16:33 +0300

    Move all notification logic to BrokerNotificationService

--- src/main/java/ua/nix/multiconfback/service/BrokerNotificationService.java
+++ src/main/java/ua/nix/multiconfback/service/BrokerNotificationService.java
@@ -8,9 +8,19 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.ExchangeBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ua.nix.multiconfback.api.model.TodoItemDto;
+import ua.nix.multiconfback.api.model.TodoListDto;
+import ua.nix.multiconfback.sockets.messages.ItemCompletedBody;
+import ua.nix.multiconfback.sockets.messages.ItemDeletedBody;
+import ua.nix.multiconfback.sockets.messages.ListDeletedBody;
 import ua.nix.multiconfback.sockets.messages.WsMessage;
 
 import static ua.nix.multiconfback.util.Constants.AMQ_TOPIC_PREFIX;
+import static ua.nix.multiconfback.util.Constants.ITEM_ADDED_TOPIC;
+import static ua.nix.multiconfback.util.Constants.ITEM_COMPLETED_TOPIC;
+import static ua.nix.multiconfback.util.Constants.ITEM_DELETED_TOPIC;
+import static ua.nix.multiconfback.util.Constants.LIST_ADDED_TOPIC;
+import static ua.nix.multiconfback.util.Constants.LIST_DELETED_TOPIC;
 import static ua.nix.multiconfback.util.Constants.RECIPIENT_HEADER;
 
 @Slf4j
@@ -46,6 +56,70 @@ public class BrokerNotificationService {
                 .withBody(socketMessage)
                 .build();
         producerTemplate.send(getTopicDestination(topic), exchange);
+    }
+
+    public void notifyListItemCompleted(String itemId, boolean isCompleted, boolean notifyAll) {
+        WsMessage message = WsMessage.builder()
+                .topic(ITEM_COMPLETED_TOPIC)
+                .data(ItemCompletedBody.builder()
+                        .itemId(itemId)
+                        .isCompleted(isCompleted)
+                        .build())
+                .build();
+        notify(message, notifyAll);
+    }
+
+    public void notifyListItemAdded(TodoItemDto itemDto, boolean notifyAll) {
+        WsMessage message = WsMessage.builder()
+                .topic(ITEM_ADDED_TOPIC)
+                .data(itemDto)
+                .build();
+        notify(message, notifyAll);
+    }
+
+    public void notifyListItemDeleted(String listId, String itemId, boolean notifyAll) {
+        WsMessage message = WsMessage.builder()
+                .topic(ITEM_DELETED_TOPIC)
+                .data(ItemDeletedBody.builder()
+                        .listId(listId)
+                        .itemId(itemId)
+                        .build())
+                .build();
+        notify(message, notifyAll);
+    }
+
+    public void notifyListDeleted(String listId, boolean isPublic) {
+        WsMessage message = WsMessage.builder()
+                .topic(LIST_DELETED_TOPIC)
+                .data(new ListDeletedBody(listId, isPublic))
+                .build();
+        if (isPublic) {
+            notifyAll(message);
+        } else {
+            notifyCurrentUser(message);
+        }
+    }
+
+    public void notifyListAdded(TodoListDto list, boolean notifyAll) {
+        WsMessage message = WsMessage.builder()
+                .topic(LIST_ADDED_TOPIC)
+                .data(list)
+                .build();
+        notify(message, notifyAll);
+    }
+
+
+    private void notify(WsMessage message, boolean notifyAll) {
+        if (notifyAll) {
+            notifyAll(message);
+        } else {
+            notifyCurrentUser(message);
+        }
+    }
+
+    private void notifyCurrentUser(WsMessage message) {
+        String username = authService.getCurrentUser().getUsername();
+        notifyUser(message, username);
     }
 
     private String getTopicDestination(String topic) {
