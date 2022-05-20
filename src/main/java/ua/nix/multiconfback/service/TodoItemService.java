aeb25503cd49e4e20f3e64eea2c330d7d2e50521
Roman Dashkivskyy <mineholst@gmail.com>
2022-05-21 01:53:56 +0300

    Refactoring - clean-up

--- src/main/java/ua/nix/multiconfback/service/TodoItemService.java
+++ src/main/java/ua/nix/multiconfback/service/TodoItemService.java
@@ -4,13 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.nix.multiconfback.model.TodoItem;
-import ua.nix.multiconfback.model.TodoList;
 
 @Service
 @Transactional
 public interface TodoItemService extends CrudRepository<TodoItem, String> {
-
-    Iterable<TodoItem> findAllByListId(String listId);
-    Iterable<TodoItem> findAllByList(TodoList list);
-
 }
