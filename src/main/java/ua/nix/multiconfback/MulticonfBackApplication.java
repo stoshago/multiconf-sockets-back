52d95287750111835bbca587032a83c7a9a38c99
Roman Dashkivskyy <mineholst@gmail.com>
2022-05-21 00:35:42 +0300

    Refactoring - remove redundant annotation @EnableJpaRepositories

--- src/main/java/ua/nix/multiconfback/MulticonfBackApplication.java
+++ src/main/java/ua/nix/multiconfback/MulticonfBackApplication.java
@@ -3,12 +3,10 @@ package ua.nix.multiconfback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableJpaRepositories
 @EnableJpaAuditing
 public class MulticonfBackApplication {
 
