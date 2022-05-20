d9d50e99c90e9f331a3a85907d363070e648c9c1
Roman Dashkivskyy <mineholst@gmail.com>
2022-05-21 00:53:52 +0300

    Refactoring - remove unused code

--- src/main/java/ua/nix/multiconfback/exceptions/DataNotFoundException.java
+++ src/main/java/ua/nix/multiconfback/exceptions/DataNotFoundException.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class DataNotFoundException extends RuntimeException {
 
-    public DataNotFoundException() {
-        super();
-    }
-
     public DataNotFoundException(String msg) {
         super(msg);
     }
